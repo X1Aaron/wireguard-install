fa987da1e2be7cbfb5246bbc2b290891cd0a3caf
aaronstuder <aaronstuder@gmail.com>
2019-02-12 06:19:33 -0500

    Update wireguard-setup.sh

--- wireguard-setup.sh
+++ wireguard-setup.sh
@@ -5,9 +5,7 @@ function installQuestions () {
 	echo "The git repository is available at: https://github.com/xxx/wireguard-install"
 	echo ""
 
-PUBLIC_IP=`curl -s ifconfig.me`
-echo "Public IP: $PUBLIC_IP"
-echo "PUBLIC_IP=$PUBLIC_IP" >> /etc/wireguard/wg.conf
+
 
 	echo ""
 	echo "Okay, that was all I needed. We are ready to setup your wireguard server now."
@@ -17,8 +15,9 @@ echo "PUBLIC_IP=$PUBLIC_IP" >> /etc/wireguard/wg.conf
 
 function installWireGuard () {
 installQuestions
-apt update -y
-apt upgrade -y
+PUBLIC_IP=`curl -s ifconfig.me`
+echo "Public IP: $PUBLIC_IP"
+echo "PUBLIC_IP=$PUBLIC_IP" >> /etc/wireguard/wg.conf
 ufw allow ssh
 ufw allow 51820/udp
 ufw --force enable
@@ -33,10 +32,13 @@ echo 1 > /proc/sys/net/ipv4/ip_forward
 
 umask 077
 mkdir /etc/wireguard/server/
+
+wg genkey | tee /etc/wireguard/server/private_key | wg pubkey > /etc/wireguard/server/public_key
+SERVER_PUBLIC_KEY=`cat `/etc/wireguard/server/public_key`
+echo "SERVER_PUBLIC_KEY: $SERVER_PUBLIC_KEY"
+echo "SERVER_PUBLIC_KEY=$SERVER_PUBLIC_KEY" >> /etc/wireguard/wg.conf
+
 mkdir /etc/wireguard/clients/
-wg genkey | tee /etc/wireguard/keys/server/private_key | wg pubkey > /etc/wireguard/keys/server/public_key
-
-
 
 echo "[Interface]" >> /etc/wireguard/wg0.conf
 echo "Address = 192.168.5.1/24" >> /etc/wireguard/wg0.conf
@@ -53,10 +55,14 @@ create_client
 function create_client () {
 echo Client Name?
 read CLIENT_NAME
+echo IP Address?
+read CLIENT_IP
 wg genkey | tee /etc/wireguard/keys/clients/$CLIENT_NAME/private_key | wg pubkey > /etc/wireguard/keys/clients/$CLIENT_NAME/public_key
+CLIENT_PRIVATE_KEY=`cat /etc/wireguard/keys/clients/$CLIENT_NAME/private_key`
+CLIENT_PUBLIC_KEY=`cat /etc/wireguard/keys/clients/$CLIENT_NAME/public_key
 echo "[Interface]" >> /etc/wireguard/clients/$CLIENT_NAME.conf
-echo "Address = 192.168.5.2/32" >> /etc/wireguard/clients/$CLIENT_NAME.conf
-echo "PrivateKey = <CLIENT_PRIVATE_KEY>" >> /etc/wireguard/clients/$CLIENT_NAME.conf
+echo "Address = $CLIENT_IP/32" >> /etc/wireguard/clients/$CLIENT_NAME.conf
+echo "PrivateKey = $CLIENT_PRIVATE_KEY" >> /etc/wireguard/clients/$CLIENT_NAME.conf
 echo "" >> /etc/wireguard/clients/$CLIENT_NAME.conf
 echo "[Peer]" >> /etc/wireguard/clients/$CLIENT_NAME.conf
 echo "PublicKey = <SERVER_PUBLIC_KEY>" >> /etc/wireguard/clients/$CLIENT_NAME.conf
@@ -65,7 +71,7 @@ echo "AllowedIPs = 0.0.0.0/0" >> /etc/wireguard/clients/$CLIENT_NAME.conf
 echo "" >> /etc/wireguard/wg0.conf
 echo "[Peer]" >> /etc/wireguard/wg0.conf
 echo "PublicKey = <CLIENT_PUBLIC_KEY>" >> /etc/wireguard/wg0.conf
-echo "AllowedIPs = 192.168.5.2/32" >> /etc/wireguard/wg0.conf
+echo "AllowedIPs = $CLIENT_IP" >> /etc/wireguard/wg0.conf
 wg-quick down wg0
 wg-quick up wg0
 }
