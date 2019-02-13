fb521bbc3e0acd2011cac9a281b068b96dbda3f1
aaronstuder <aaronstuder@gmail.com>
2019-02-13 16:20:25 -0500

    Update wireguard-install.sh

--- wireguard-install.sh
+++ wireguard-install.sh
@@ -83,7 +83,7 @@ echo "" >> /etc/wireguard/wg0.conf
 echo "# $1" >> /etc/wireguard/wg0.conf
 echo "[Peer]" >> /etc/wireguard/wg0.conf
 echo "PublicKey = $CLIENT_PUBLIC_KEY" >> /etc/wireguard/wg0.conf
-echo "AllowedIPs = $2/32" >> /etc/wireguard/wg0.conf
+echo "AllowedIPs = $2/32,::/0" >> /etc/wireguard/wg0.conf
 echo ""
 echo "Restarting WireGuard..."
 echo ""
